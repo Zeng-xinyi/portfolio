3af596ad2eefbf3c3196f57d6d268d99797b5a7a
Zeng-xinyi <134284442+Zeng-xinyi@users.noreply.github.com>
2025-10-16 11:44:35 -0700

    automatic nav

--- global.js
+++ global.js
@@ -3,3 +3,31 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'CV' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/Zeng-xinyi', title: 'GitHub' }
+];
+
+// Determine base path for links
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/"; 
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
