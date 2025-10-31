db35c966d4f16dc2695f41e84e273779db9167f8
Zeng-xinyi <134284442+Zeng-xinyi@users.noreply.github.com>
2025-10-31 03:50:58 -0700

    sector and tags

--- projects/projects.js
+++ projects/projects.js
@@ -15,22 +15,66 @@ if (titleElement) {
   titleElement.textContent = `${count} Projects`;
 }
 
-// lab5 step 1.3: Draw a full circle with D3
-// Select the existing <svg> (lab5 step 1.1)
-const svg = d3.select('#projects-pie-plot');
-
+// // lab5 step 1.3: Draw a full circle with D3
+// // Select the existing <svg> (lab5 step 1.1)
+// const svg = d3.select('#projects-pie-plot');
+// 
 // defines how the path should look
-const arcGenerator = d3.arc()
-  .innerRadius(0)   
-  .outerRadius(50); 
+// const arcGenerator = d3.arc()
+//   .innerRadius(0)   
+//   .outerRadius(50); 
+// 
+// // Generate one full circle: 0 -> 2pai
+// const arcPath = arcGenerator({
+//   startAngle: 0,
+//   endAngle: 2 * Math.PI
+// });
+// 
+// // Append the path to SVG
+// svg.append('path')
+//   .attr('d', arcPath)
+//   .attr('fill', 'red');
 
-// Generate one full circle: 0 -> 2pai
-const arcPath = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI
+// Draw a static pie chart with D3
+const svg = d3.select('#projects-pie-plot');
+const arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+const sliceGenerator = d3.pie().value(d => d.value);
+
+const data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
+const arcData = sliceGenerator(data);
+const colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+arcData.forEach((d, idx) => {
+  svg.append('path')
+    .attr('d', arcGenerator(d))
+    .attr('fill', colors(idx))
+    .attr('stroke', 'white')
+    .attr('stroke-width', 0.5);
 });
 
-// Append the path to SVG
-svg.append('path')
-  .attr('d', arcPath)
-  .attr('fill', 'red');
+
+
+
+
+
+
+
+// avoid repeating rendering
+d3.select('.legend').html('');
+
+const legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend.append('li')
+    .attr('class', 'legend-item')
+    .attr('style', `--color: ${colors(idx)}`)
+    .html(`<span class="swatch" aria-hidden="true"></span> ${d.label} <em>(${d.value})</em>`);
+});
